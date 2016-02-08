518753334d43ac468ac05288aa8b9a2d43e34ddb
Mark Evans <mark.evans@teamsnap.com>
2016-02-07 18:05:45 -0700

    Adds ability to bump payment at specific month

--- credit_card.exs
+++ credit_card.exs
@@ -1,5 +1,6 @@
 defmodule CreditCard do
-  def balance_over_months(balance,_,0), do: IO.puts "$#{balance}, DONE!"
+  def balance_over_months(balance,_,0),
+  do: IO.puts "$#{balance}, DONE!"
   def balance_over_months(balance, apr, months) do
     balance
       |> monthly_percentage(apr)
@@ -7,7 +8,8 @@ defmodule CreditCard do
       |> balance_over_months(apr, months - 1)
   end
 
-  def balance_over_months_with_payment(_,_,0,_), do: IO.puts "DONE!"
+  def balance_over_months_with_payment(_,_,0,_),
+  do: IO.puts "DONE!"
   def balance_over_months_with_payment(balance, apr, months, payment) do
     balance
       |> monthly_percentage(apr, payment)
@@ -19,17 +21,36 @@ defmodule CreditCard do
   def months_to_payoff(balance, apr, payment, months) when balance > 0 do
     balance
       |> monthly_percentage(apr, payment)
+      |> print_result(months)
       |> months_to_payoff(apr, payment, months + 1)
   end
-  def months_to_payoff(_,_,_,months), do: IO.puts "#{months} months to payoff"
+  def months_to_payoff(_,_,_,months),
+  do: IO.puts "#{months} months to payoff"
 
-  defp daily_percentage_rate(apr), do: apr / 365
+  def months_to_payoff_w_bump(balance, apr, payment, %{:at_month => at_month, :amount => amount}, months) when at_month == months,
+  do: months_to_payoff_w_bump(balance, apr, payment + amount, months)
+  def months_to_payoff_w_bump(balance, apr, payment, %{:at_month => at_month, :amount => amount}, months) do
+    balance
+      |> monthly_percentage(apr, payment)
+      |> print_result(months)
+      |> months_to_payoff_w_bump(apr, payment, %{:at_month => at_month, :amount => amount}, months + 1)
+  end
+  def months_to_payoff_w_bump(balance, apr, payment, months) when balance > 0,
+  do: months_to_payoff(balance, apr, payment, months)
 
-  defp monthly_percentage(balance, apr), do: monthly_percentage(balance, apr, 0)
+  defp daily_percentage_rate(apr),
+  do: apr / 365
+
+  defp monthly_percentage(balance, apr),
+  do: monthly_percentage(balance, apr, 0)
   defp monthly_percentage(balance, apr, payment) do
-    Float.ceil(
-      (balance * (daily_percentage_rate(apr) * 30.42) + balance) - payment, 2
-    )
+    apr
+      |> daily_percentage_rate
+      |> Kernel.*(30.42)
+      |> Kernel.*(balance)
+      |> Kernel.+(balance)
+      |> Kernel.-(payment)
+      |> Float.ceil(2)
   end
 
   defp print_result(amount, months) do
